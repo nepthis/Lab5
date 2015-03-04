78dec6d770fa7887d38854cbc9a5d4731d81261c
aleola <alexanderolausson@hotmail.se>
2015-03-04 15:53:01 +0100

    Update CarWashEventStart.java

--- CarWashEventStart.java
+++ CarWashEventStart.java
@@ -7,10 +7,32 @@ public class CarWashEventStart extends Event {
 	}
 	
 	/**
-	 * I CarWashEventStarts execute metod generar det första arrive eventet
-	 * samt printa ut de ursprungliga värdena
+	 * I CarWashEventStarts execute metod generar det fÃ¶rsta arrive eventet
+	 * samt printa ut de ursprungliga vÃ¤rdena
 	 */
 	public void execute(Simulator sim, SimState ss) {
+		CarWashArrive(ss.arrivalRand());
+//		CarWashState ss3 = new CarWashState();
+//		int FastMax = ss3.fastWashMax;
+//		int SlowMax = ss3.slowWashMax;
+//		int QueueMax = ss3.carQueueMax;
+//		
+//		int FastMax = ss.fastWashMax;
+//		int SlowMax = ss.slowWashMax;
+//		int QueueMax = ss.carQueueMax;
+		
+		System.out.println("Fast machines: " + FastMax);
+		System.out.println("Slow machines: " + SlowMax);
+		
+		System.out.println("Fast distribution: " + "(2,8, 4,6)");
+		System.out.println("Slow distribution: " + "(3,5, 6,7)");
+		System.out.println("Exponential distribution with lambda = 2,0");
+		System.out.println("Seed = 1234");
+		System.out.println("--------------------------------------------");
+		
+		
+		System.out.println("Fast machines: " + QueueMax);
+		
 		
 	}
 	
