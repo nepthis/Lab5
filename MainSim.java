6ff290033411e45a8928c74106f3848fed3a1e3c
Grantomten <Grantomten@users.noreply.github.com>
2015-03-09 13:24:20 +0100

    Update MainSim.java

--- MainSim.java
+++ MainSim.java
@@ -1,8 +1,11 @@
 package lab5;
 
+import lab5.carwash.*;
+import lab5.simulator.Simulator;
+
 public class MainSim {
 	public static void main(String[] args) {
-		Simulator sim = new Simulator();
+		Simulator sim = new Simulator(new CarWashState(2,2,5, 2.8,4.6, 3.5, 6.7, 1234l, 2d), new CarView(), new CarWashEventStart(0d), new CarWashEventStop(15d));
 		sim.mainLoop();
 	}
 }
