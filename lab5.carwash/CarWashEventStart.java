6844b6d44348da59c73eaa0734e599fdb0f0c848
aleola <alexanderolausson@hotmail.se>
2015-03-09 14:12:10 +0100

    Update CarWashEventStart.java

--- lab5.carwash/CarWashEventStart.java
+++ lab5.carwash/CarWashEventStart.java
@@ -4,14 +4,20 @@ import lab5.simulator.SimState;
 import lab5.simulator.Simulator;
 
 public class CarWashEventStart extends CarWashEvent {
+	/**
+	 * The class CarWashEventStart starts the carwash by creating the first arrive event 
+	 * 
+	 */
+	
 	
 	public CarWashEventStart(double p) {
 		super(p, -1);
 	}
 	
 	/**
-	 * I CarWashEventStarts execute metod generar det första arrive eventet
-	 * samt printa ut de ursprungliga värdena
+	 * 
+	 * The execute method is generating the first arrive event
+	 * 
 	 */
 	public void execute(Simulator sim, SimState ss) {
 		CarWashState ss2 = (CarWashState) ss;
@@ -20,8 +26,14 @@ public class CarWashEventStart extends CarWashEvent {
 		sim.addEvent(new CarWashEventArrive(ss2.getNextArrival(), ss2.getNextCarId()));
 		
 	}
-	
+
+
+	/**
+	 * 
+	 * 
+	 */
 	public String toString() {
+
 		return "Start";
 	}
 }
