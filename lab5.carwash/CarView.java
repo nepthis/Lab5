61390af25ac160d29c4a35e8656a44c5e9cf974a
Grantomten <Grantomten@users.noreply.github.com>
2015-03-09 13:59:13 +0100

    Update CarView.java

--- lab5.carwash/CarView.java
+++ lab5.carwash/CarView.java
@@ -1,5 +1,6 @@
 package lab5.carwash;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 import lab5.simulator.SimView;
@@ -14,22 +15,24 @@ public class CarView extends SimView {
     	CarWashEvent cwe = (CarWashEvent) e;
     	
     	if(cwe.toString() == "Start") {
-    		System.out.println("Fast machines: " + cws.fastWashMax);
-    		System.out.println("Slow machines: " + cws.slowWashMax);
+    		System.out.println("Fast machines: " + cws.getFastWashMax());
+    		System.out.println("Slow machines: " + cws.getSlowWashMax());
     		System.out.println("Fast distribution: " + "(" + cws.fastDist1+ ", "+ cws.fastDist2+")");
     		System.out.println("Slow distribution: " + "(" + cws.slowDist1+ ", "+ cws.slowDist2+")");
     		System.out.println("Exponential distribution with lambda = "+ cws.lambda);
     		System.out.println("Seed = "+cws.seed);
-    		System.out.println("Max Queue size: " + cws.carQueueMax);
+    		System.out.println("Max Queue size: " + cws.getCarQueueMax());
     		System.out.println("--------------------------------------");
     		System.out.format("%-8s%-8s%-8s%-8s%-10s%-14s%-14s%-14s%-10s\n", "Time", "Fast", "Slow", "Id", "Event", "IdleTime", "QueueTime", "QueueSize", "Rejected");
     	}
-
+    	ArrayList<Integer> fastWash = cws.getFastWashQueue();
+    	ArrayList<Integer> slowWash = cws.getSlowWashQueue();
+    	ArrayList<Integer> carQueue = cws.getCarQueue();
 
     	System.out.format("%-8.2f %-7d %-7d%-8s%-10s  %-12.2f  %-12.2f   %-11d   %-7d\n",
-    			cws.time, cws.fastWashMax-cws.fastWash.size(), cws.slowWashMax-cws.slowWash.size(),
+    			cws.getTime(), cws.getFastWashMax()-fastWash.size(), cws.getSlowWashMax() - slowWash.size(),
     			(cwe.getId() == -1 ? "-" : Integer.toString(cwe.getId())),
-    			cwe, cws.idleTime, cws.queueTime, cws.carQueue.size(), cws.rejectedCars);
+    			cwe, cws.getIdleTime(), cws.getQueueTime(), carQueue.size(), cws.getRejectedCars());
     	
         
     	if(cwe.toString() == "Stop") {
@@ -38,7 +41,7 @@ public class CarView extends SimView {
     				"Total queueing time: %.2f\n" +
     				"Mean queueing time: %.2f\n" +
     				"Rejected cars: %d", 
-    				cws.idleTime, cws.queueTime, cws.queueTime/(cws.counter-cws.rejectedCars), cws.rejectedCars);
+    				cws.getIdleTime(), cws.getQueueTime(), cws.getQueueTime()/(cws.getTotalCars()-cws.getRejectedCars()), cws.getRejectedCars());
     	}	
     }
 }
