9f4ad89c830078448a4829c5c8c5f742773e4f81
Grantomten <Grantomten@users.noreply.github.com>
2015-03-09 08:22:42 +0100

    Update CarWashEventStart.java

--- CarWashEventStart.java
+++ CarWashEventStart.java
@@ -16,14 +16,14 @@ public class CarWashEventStart extends Event {
 		
 		System.out.println("Fast machines: " + ss2.fastWashMax);
 		System.out.println("Slow machines: " + ss2.slowWashMax);
+		System.out.println("Queue size: " + ss2.carQueueMax);
 		
 		System.out.println("Fast distribution: " + "(2,8, 4,6)");
 		System.out.println("Slow distribution: " + "(3,5, 6,7)");
 		System.out.println("Exponential distribution with lambda = 2,0");
 		System.out.println("Seed = 1234");
 		System.out.println("--------------------------------------------");
-		
-		System.out.println("Fast machines: " + ss2.carQueueMax);
+
 		
 		ss2.setChanged();
 		ss2.notifyObservers();
