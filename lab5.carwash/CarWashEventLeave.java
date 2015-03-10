311fa917a26485f6d24a442f5d5a609151258e4f
jonolo-3 <jonolo-3@student.ltu.se>
2015-03-10 23:33:05 +0100

    Update CarWashEventLeave.java

--- lab5.carwash/CarWashEventLeave.java
+++ lab5.carwash/CarWashEventLeave.java
@@ -13,20 +13,16 @@ public class CarWashEventArrive extends CarWashEvent {
 	}
 	
 	/**
-	 * Timevariabeln i statevariabeln ökas med detta eventets tid minus timevariabeln.
+	 * Ökar tids- och idletidvariabeln. Söker igenom den snabba och sedan långsamma tvätten om id:t för bilen finns.
+	 * Om den gör det, ta bort den och lägga till första id:t från bilkön(om det finns) i en en tvätt, med högre priorietet
+	 * på snabb tvätt. Sedan genereras ett nytt leave event för det id:t
 	 * 
-	 * Finns det några lediga biltvättar måste även tidigare nämnd deltatid gångras med antal lediga tvättar,
-	 * och plussas på i idleTime i statevariabeln.
-	 * 
-	 * Sedan läggs en bil till i den snabba eller långsamma biltvätten.
-	 * Den snabba prioriteras över den långsamma. Finns ingen ledig plats läggs den till i kön.
-	 * Lades en bil till i en tvätt, måste ett leave-event genereras med prioriteten som fås av
-	 * ss.slowWashRand.next() om den ligger i en långsam biltvätt eller ss.fastWashRand.next() om
-	 * den ligger i en snabb biltvätt.
-	 * Är kön i sin tur full ska rejectedCars i statevariabeln ökas.
-	 * 
-	 * Sist skall ett nytt arrive genereras med prioriteten som fås av ss.arrivalRand() och funktionen avslutas.
+	 *  @param sim Simulator shell.
+	 *  @param ss CarWashState
+	 *  @return void
 	 */
+	 
+	 //FEL KOD HÄRRRRR
 	public void execute(Simulator sim, SimState ss) {
 		CarWashState cws = (CarWashState) ss;
 		cws.increaseTotalCars();
