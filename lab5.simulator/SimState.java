d747e225a9ee24fad95c8c880ef2b60ccd03b0ce
Grantomten <Grantomten@users.noreply.github.com>
2015-03-11 09:10:17 +0100

    Update SimState.java

--- lab5.simulator/SimState.java
+++ lab5.simulator/SimState.java
@@ -2,17 +2,32 @@ package lab5.simulator;
 
 import java.util.Observable;
 
+/**
+ * The skeleton state class. It contains the boolean variable that decides it the
+ * simulation should stop, and two functions that checks the variable or sets it to true.
+ * */
 public class SimState extends Observable {
 	protected boolean stop;
 	
+	/**
+	 * Initializes the stop boolean to false.
+	 * */
 	public SimState() {
 		stop = false;
 	}
 	
+	/**
+	 * Returns the value of the stop variable.
+	 * @return boolean
+	 * */
 	public boolean hasStopped() {
 		return stop;
 	}
 	
+	/**
+	 * Sets the boolean stop to true, effectively ending the simulation.
+	 * @return void
+	 * /*
 	public void setStopped() {
 		stop = true;
 	}
