4f4c2fd95eedbfd878f8e78d077be18078d60f11
Grantomten <Grantomten@users.noreply.github.com>
2015-03-11 09:08:15 +0100

    Update SimView.java

--- lab5.simulator/SimView.java
+++ lab5.simulator/SimView.java
@@ -2,6 +2,11 @@ package lab5.simulator;
 
 import java.util.Observer;
 
+/**
+ * A very bare bones class since it really doesn't have to do anything. Its extender
+ * should implement the update from the Observer class which will be called on by the 
+ * Observable state.
+ * */
 public abstract class SimView implements Observer {
 	public SimView() {
 		
